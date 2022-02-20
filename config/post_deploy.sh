17e2267cb397cc0710c1b1308d02107f825a2b67
Stephen Yeargin <stephen.yeargin@gmail.com>
2022-02-20 00:35:16 -0600

    Drop in favor of GitHub pages

--- config/post_deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source ~/.bash_profile
-
-bundle install
-bundle exec jekyll build
-bundle exec rake notify
--- config/post_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export RBENV_VERSION=2.1.1
-
-source ~/.bash_profile
-
-ruby --version
-
-bundle install --path=vendor/bundle
